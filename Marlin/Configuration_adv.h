43ace24c286e92fe70bd74d9ccaf79765125faa3
“ils15” <igorleite-ils@hotmail.com>
2020-09-03 23:49:39 -0300

    update

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2463,7 +2463,7 @@
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
   #define E0_HYBRID_THRESHOLD    100
-  #define E1_HYBRID_THRESHOLD     30
+  #define E1_HYBRID_THRESHOLD    100
   #define E2_HYBRID_THRESHOLD     30
   #define E3_HYBRID_THRESHOLD     30
   #define E4_HYBRID_THRESHOLD     30
